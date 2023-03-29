a71166af97d038ab7470364b7ddff438b003d683
thanhngan22 <nganbui9a2017@gmail.com>
2023-03-29 23:10:51 +0700

    init tasks

---
+++ moodle/TH/Week06/21120505@6.sql
@@ -0,0 +1,142 @@
+use QLGV
+go
+
+-- Viết các câu truy vấn lồng nâng cao --
+
+-- Viết theo 3 cách except, not exists và count --
+
+-- Q58: Cho biets tên giáo viên nào mà tham gia đề tài đủ tất cả các chủ đề
+
+-- C1:
+
+-- C2:
+
+-- C3:
+
+-- Q59: Cho biết tên đề tài nào mà được tất cả các giáo viên của bộ môn HTTT tham gia
+
+-- C1:
+
+-- C2:
+
+-- C3:
+
+-- Q60: Cho biết tên đề tài có tất cả giảng viên bộ môn "Hệ thống thông tin tham gia"
+
+-- C1:
+
+-- C2:
+
+-- C3:
+
+-- Q61: Cho biết giáo viên nào đã tham gia tất cả các đề tài có mã chủ đề là QLGD
+
+-- C1:
+
+-- C2:
+
+-- C3:
+
+-- Q62: Cho biết tên giáo viên nào tham gia tất cả các đề tài mà giáo viên Trần Trà Hương đã tham gia
+
+-- C1:
+
+-- C2:
+
+-- C3:
+
+-- Q63
+
+-- C1:
+
+-- C2:
+
+-- C3:
+
+-- Q64
+
+-- C1:
+
+-- C2:
+
+-- C3:
+
+-- Q65
+
+-- C1:
+
+-- C2:
+
+-- C3:
+
+-- Q66
+
+-- C1:
+
+-- C2:
+
+-- C3:
+
+-- Q67
+
+-- C1:
+
+-- C2:
+
+-- C3:
+
+-- Q68
+
+-- C1:
+
+-- C2:
+
+-- C3:
+
+-- Q69
+
+-- C1:
+
+-- C2:
+
+-- C3:
+
+-- Q70
+
+-- C1:
+
+-- C2:
+
+-- C3:
+
+-- Q71
+
+-- C1:
+
+-- C2:
+
+-- C3:
+
+-- Q72
+
+-- C1:
+
+-- C2:
+
+-- C3:
+
+-- Q73
+
+-- C1:
+
+-- C2:
+
+-- C3:
+
+-- Q74
+
+-- C1:
+
+-- C2:
+
+-- C3:
